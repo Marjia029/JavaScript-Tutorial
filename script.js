9026cea91018a956a2c84c7fad4dad910ae80354
Marjia <marjiaafroj2@gmail.com>
2024-09-02 01:06:36 +0600

    changing text color function is added

--- script.js
+++ script.js
@@ -3,3 +3,7 @@
 function myFunction(){
     document.getElementById('lorem_text').innerHTML = 'Hello World! The text is changed. Ta Daa!!!';
 }
+
+function changeTextColor(){
+    document.getElementsByTagName('p')[0].style.color = 'red';
+}
