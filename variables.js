0b9f34435052f21680d9fa921c86e7081f9d6200
Marjia <marjiaafroj2@gmail.com>
2024-09-02 13:02:45 +0600

    variables file is added

---
+++ variables.js
@@ -0,0 +1,13 @@
+// defining a number variable
+let number = 5; // let is used for declaring a variable
+//defining a string variable
+let string = "hello";
+
+console.log(number, string);
+
+// updating the value of the variable
+
+number = number + 10;
+string = string + " world";
+
+console.log(number, string);
